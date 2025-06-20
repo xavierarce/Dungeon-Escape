e4c062dc4427f966c4a174cefb08dae6c3999ead
Xavier Arce <xavierarce54321@gmail.com>
2025-06-20 10:47:06 +0200

    Player selection

--- game/GameManager.java
+++ game/GameManager.java
@@ -17,12 +17,37 @@ public class GameManager {
     System.out.print("Enter your name: ");
     String name = scanner.nextLine();
 
-    System.out.println("Choose your class: [Warrior, Assassin, Tank]");
-    String type = scanner.nextLine();
+    String[] classes = { "Warrior", "Assassin", "Tank" };
+    int selected = 0;
 
+    while (true) {
+      System.out.println("\nChoose your class (use 'w' to move up, 's' to move down, press Enter to confirm):");
+      for (int i = 0; i < classes.length; i++) {
+        if (i == selected) {
+          System.out.println("> " + classes[i]);
+        } else {
+          System.out.println("  " + classes[i]);
+        }
+      }
+
+      String input = scanner.nextLine();
+
+      if (input.equalsIgnoreCase("w")) {
+        selected = (selected - 1 + classes.length) % classes.length;
+      } else if (input.equalsIgnoreCase("s")) {
+        selected = (selected + 1) % classes.length;
+      } else if (input.isEmpty()) { // juste appuyer sur Entrée pour valider
+        break;
+      } else {
+        System.out.println("Invalid input. Use 'w', 's', or press Enter.");
+      }
+    }
+
+    String type = classes[selected];
     player = new Player(name, type);
-    System.out.println("Welcome " + player.getName() + " the " + type + "!");
+    System.out.println("\nWelcome " + player.getName() + " the " + type + "!");
 
+    // Boucle principale du jeu inchangée
     while (player.isAlive()) {
       dungeonCount++;
       System.out.println("\n--- Dungeon " + dungeonCount + " ---");
